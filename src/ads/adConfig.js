92faafc99309e6e25a8862be317b89d50ef164c7
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2026-06-30 16:36:32 -0400

    Add live AdSense slot to marketplace rails

--- src/ads/adConfig.js
+++ src/ads/adConfig.js
@@ -2,6 +2,6 @@ export const ADSENSE_CLIENT_ID = 'ca-pub-5061195174390655'
 export const ADS_ENABLED = true
 
 export const MARKETPLACE_AD_SLOTS = {
-  leftRail: '',
-  rightRail: ''
+  leftRail: '6816859206',
+  rightRail: '6816859206'
 }
